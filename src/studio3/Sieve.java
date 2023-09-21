ffdc510483a2e200d9b1a41d964a8dc7293d1af9
alaynarodgers <alaynarodgers@localhost>
2023-09-21 12:49:00 -0500

    finished studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,23 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("What is n?");
+		int n = in.nextInt();
+		
+		boolean[] sieveArray = new boolean[n];
+		sieveArray = true;
+		
+		for(int i = 2; i <= Math.sqrt(n); i++) {
+			if A[i] = true {
+				for( int j = i)
+			}
+		}
+	}
+
+}
